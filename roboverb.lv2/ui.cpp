7355f62b69698927f98798e88f6988634f073fdd
Michael Fisher <mfisher@kushview.net>
2023-08-08 01:59:03 -0400

    ui: add toggles ui: bind controls to lvtk::UI::write

--- roboverb.lv2/ui.cpp
+++ roboverb.lv2/ui.cpp
@@ -21,10 +21,10 @@
 #include <iostream>
 
 #include <lvtk/ui.hpp>
-#include <lvtk/ui/opengl.hpp>
-#include <lvtk/ui/widget.hpp>
 #include <lvtk/ui/button.hpp>
+#include <lvtk/ui/opengl.hpp>
 #include <lvtk/ui/slider.hpp>
+#include <lvtk/ui/widget.hpp>
 
 #include <lvtk/ext/idle.hpp>
 #include <lvtk/ext/parent.hpp>
@@ -39,16 +39,15 @@
 
 using namespace lvtk;
 
-
 class RoboverbToggle : public lvtk::Button {
 public:
-    RoboverbToggle() = default;
+    RoboverbToggle()  = default;
     ~RoboverbToggle() = default;
 
 protected:
     void paint_button (Graphics& g, bool highlight, bool down) override {
         g.set_color (0x000000ff);
-        auto b = bounds().at(0);
+        auto b = bounds().at (0);
         g.fill_rect (b);
 
         g.set_color (toggled() ? 0xff000ff : 0x333333ff);
@@ -59,27 +58,44 @@ protected:
 
 class RoboverbContent : public lvtk::Widget {
 public:
+    std::function<void (uint32_t, float)> on_control_changed;
+
     RoboverbContent() {
         set_opaque (true);
 
-        for (int i = 0; i < 5; ++i) {
+        for (int i = RoboverbPorts::Wet; i <= RoboverbPorts::Width; ++i) {
             auto s = add (new lvtk::Slider());
             s->set_range (0.0, 1.0);
             s->set_type (Slider::VERTICAL_BAR);
+            s->on_value_changed = [&, i, s]() {
+                if (on_control_changed) {
+                    const auto port  = static_cast<uint32_t> (i);
+                    const auto value = static_cast<float> (s->value());
+                    on_control_changed (port, value);
+                }
+            };
             sliders.push_back (s);
         }
 
-        for (int i = 0; i < 12; ++i) {
-            // auto t = add (new RoboverbToggle());
-            // t->toggle (false);
-            // toggles.push_back (t);
+        for (int i = RoboverbPorts::Comb_1; i <= RoboverbPorts::AllPass_4; ++i) {
+            auto t = add (new RoboverbToggle());
+            t->toggle (false);
+            t->on_clicked = [&, i, t]() {
+                t->toggle (! t->toggled());
+                if (on_control_changed) {
+                    const auto port = static_cast<uint32_t> (i);
+                    on_control_changed (port, t->toggled() ? 1.f : 0.f);
+                }
+            };
+
+            toggles.push_back (t);
         }
 
         show_all();
         set_size (640, 360);
     }
 
-    ~RoboverbContent()  {
+    ~RoboverbContent() {
         for (auto t : toggles)
             delete t;
         toggles.clear();
@@ -89,14 +105,13 @@ public:
     }
 
     void update_toggle (int index, bool value) {
-        
         if (! (index >= 0 && index < (int) toggles.size()))
             return;
         // std::clog << "[roboverb] update_toggle(" << (int)value << ")\n";
         toggles[index]->toggle (value);
     }
 
-    template<typename Ft>
+    template <typename Ft>
     void update_slider (int index, Ft value) {
         if (! (index >= 0 && index < (int) sliders.size()))
             return;
@@ -109,15 +124,26 @@ public:
 
 protected:
     void resized() override {
-        auto btn_size = height() / 3;
-        auto btn_hspace = btn_size * 4;
+        auto btn_size      = height() / 3;
+        auto btn_hspace    = btn_size * 4;
         auto slider_hspace = width() - btn_hspace;
 
-        auto sb = bounds().at(0).slice_left (slider_hspace);
-        int w = slider_hspace / 5;
+        auto sb = bounds().at (0).slice_left (slider_hspace);
+        int w   = sb.width / 5;
         for (int i = 0; i < 5; ++i) {
             auto sr = sb.slice_left (w);
             sliders[i]->set_bounds (sr.smaller (1, 0));
+        }
+
+        auto tb     = bounds().at (0).slice_right (btn_hspace).smaller (1);
+        size_t tidx = 0;
+        for (int i = 0; i < 3; ++i) {
+            auto r = tb.slice_top (btn_size);
+            for (int j = 0; j < 4; ++j) {
+                auto* t = toggles[tidx++];
+                t->set_bounds (r.slice_left (btn_size)
+                                   .smaller (1));
+            }
         }
     }
 
@@ -151,6 +177,7 @@ public:
     }
 
     void cleanup() {
+        content.reset();
     }
 
     int idle() {
@@ -158,12 +185,22 @@ public:
         return 0;
     }
 
+    bool _block_sending { false };
+
+    void send_control (uint32_t port, float value) {
+        if (_block_sending)
+            return;
+        write (port, value);
+    }
+
     void port_event (uint32_t port, uint32_t size, uint32_t format, const void* buffer) {
         if (format != 0 || size != sizeof (float))
             return;
 
-        const float value    = *((float*) buffer);
-        const bool bvalue    = value != 0.f;
+        _block_sending = true;
+
+        const float value = *((float*) buffer);
+        const bool bvalue = value != 0.f;
 
         if (port >= RoboverbPorts::Comb_1 && port <= RoboverbPorts::AllPass_4) {
             auto index = static_cast<int> (port - RoboverbPorts::Comb_1);
@@ -172,6 +209,8 @@ public:
             auto index = static_cast<int> (port - RoboverbPorts::Wet);
             content->update_slider (index, value);
         }
+
+        _block_sending = false;
     }
 
     LV2UI_Widget widget() {
@@ -179,6 +218,10 @@ public:
             content = std::make_unique<Content>();
             _main.elevate (*content, 0, (uintptr_t) parent.get());
             content->set_visible (true);
+            content->on_control_changed = std::bind (
+                &RoboverbUI::send_control, this, 
+                std::placeholders::_1,
+                std::placeholders::_2);
             if (auto view = content->find_view()) {
                 view->set_size (content->width(), content->height());
                 notify_size (content->width(), content->height());
